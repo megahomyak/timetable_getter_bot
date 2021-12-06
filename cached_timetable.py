961756b7b3ac90267abaa8058926d22998b622c9
megahomyak <g.megahomyak@gmail.com>
2021-12-06 21:05:48 +0500

    Rewrote lots of stuff, so now it will work properly (I hope)

---
+++ cached_timetable.py
@@ -0,0 +1,130 @@
+import asyncio
+import datetime
+import re
+from dataclasses import dataclass
+from io import BytesIO
+from typing import Optional
+
+import netschoolapi
+import pytz
+import vkbottle
+from PIL import Image as PILImageModule
+
+from image_cropper import ImageCropper
+
+YEKATERINBURG_TIMEZONE = pytz.timezone("Asia/Yekaterinburg")
+
+SATURDAY = 5
+
+
+class TimetableNotFound(Exception):
+    pass
+
+
+def now():
+    return datetime.datetime.now(tz=YEKATERINBURG_TIMEZONE)
+
+
+def today():
+    return now().date()
+
+
+def get_next_school_day_date():
+    today_ = today()
+    return today_ + datetime.timedelta(
+        # Skipping the sunday
+        days=2 if today_.weekday() == SATURDAY else 1
+    )
+
+
+TIMETABLE_ANNOUNCEMENT_TITLE_REGEX = re.compile(
+    r"расписание для 5-11 классов на (\d+).+", flags=re.IGNORECASE
+)
+
+
+@dataclass
+class Timetable:
+    attachment_string: str
+    date: datetime.date
+
+
+class TimetableCacher:
+
+    def __init__(
+            self, netschoolapi_client: netschoolapi.NetSchoolAPI,
+            vk_client: vkbottle.Bot):
+        self._netschoolapi_client = netschoolapi_client
+        self._vk_client = vk_client
+        self._timetable_getting_lock = asyncio.Lock()
+        self._cached_timetable: Optional[Timetable] = None
+
+    async def get_from_cache_or_download(
+            self, date: datetime.date = None):
+        if date is None:
+            date = today()
+        if self._cache_is_valid(date):
+            return self._cached_timetable
+        try:
+            return await self._download_and_cache_timetable(date)
+        except TimetableNotFound:
+            if self._cached_timetable is None:
+                raise TimetableNotFound
+            return self._cached_timetable
+
+    async def download(self, date: datetime.date = None):
+        return await self._download_and_cache_timetable(
+            date or get_next_school_day_date()
+        )
+
+    def _cache_is_valid(self, expected_date: datetime.date):
+        return (
+            self._cached_timetable is not None
+            and self._cached_timetable.date == expected_date
+        )
+
+    async def _download_and_cache_timetable(self, date: datetime.date):
+        async with self._timetable_getting_lock:
+            try:
+                timetable = await self._download_timetable(date)
+            except TimetableNotFound:
+                pass
+            else:
+                attachment_string = (
+                    await vkbottle.PhotoMessageUploader(
+                        api=self._vk_client.api
+                    ).upload(timetable)
+                )
+                self._cached_timetable = Timetable(
+                    attachment_string=attachment_string,
+                    date=date
+                )
+
+    async def _download_timetable(self, date: datetime.date) -> BytesIO:
+        next_day_number = date.day
+        for announcement in (
+            await self._netschoolapi_client.announcements()
+        ):
+            for attachment in announcement.attachments:
+                match = TIMETABLE_ANNOUNCEMENT_TITLE_REGEX.fullmatch(
+                    attachment.name
+                )
+                if match:
+                    if int(match.group(1)) == next_day_number:
+                        file_buffer = await (
+                            self._netschoolapi_client
+                                .download_attachment_as_bytes(
+                                    attachment
+                                )
+                        )
+                        image_cropper = ImageCropper(
+                            PILImageModule.open(file_buffer)
+                        )
+                        file_buffer = BytesIO()
+                        format_ = attachment.name.split(".")[-1]
+                        image_cropper.crop().save(
+                            file_buffer,
+                            format="jpeg" if format_ == "jpg" else format_
+                        )
+                        return file_buffer
+                    raise TimetableNotFound
+        raise TimetableNotFound
--- main_logic.py
+++ main_logic.py
@@ -1,41 +1,25 @@
 import asyncio
 import datetime
 import random
-import re
-import traceback
-from dataclasses import dataclass
-from io import BytesIO
-from typing import Optional
 
-import pytz
 import vkbottle
 import vkbottle.bot
 import vkbottle.dispatch.rules.bot
-from PIL import Image as PILImageModule
 from netschoolapi import NetSchoolAPI
 
+from cached_timetable import (
+    TimetableCacher, TimetableNotFound, now, today, Timetable
+)
 from config import Config
-from image_cropper import ImageCropper
 
 HELP_MESSAGE = (
     "/помощь (или \"/команды\") - это сообщение\n"
     "/расписание - последнее расписание на сегодня из сетевого города"
 )
 
-TIMETABLE_ANNOUNCEMENT_TITLE_REGEX = re.compile(
-    r"расписание для 5-11 классов на (\d+).+", flags=re.IGNORECASE
-)
 
-
-@dataclass
-class CachedTimetableInfo:
-    month_day_number: int
-    attachment_string: str
-    date: datetime.date
-
-
-class TimetableForTomorrowIsntFound(Exception):
-    pass
+async def wait_until(time: datetime.datetime):
+    await asyncio.sleep((time - now()).total_seconds())
 
 
 class PeerCheckerRule(vkbottle.dispatch.rules.bot.ABCMessageRule):
@@ -47,42 +31,23 @@ class PeerCheckerRule(vkbottle.dispatch.rules.bot.ABCMessageRule):
         return self.peer_id == message.peer_id
 
 
-def _future_done_callback(future: asyncio.Future):
-    exception = future.exception()
-    if exception:
-        # noinspection PyBroadException
-        try:
-            raise exception
-        except Exception:
-            traceback.print_exc()
-
-
 MINIMUM_TIMETABLE_SENDING_HOUR = 12
 NIGHT_HOUR = 22
 
-YEKATERINBURG_TIMEZONE = pytz.timezone("Asia/Yekaterinburg")
 
-SATURDAY = 5
-
-
-def now():
-    return datetime.datetime.now(tz=YEKATERINBURG_TIMEZONE)
+FRIDAY = 4
 
 
-def today():
-    return now().date()
-
-
-async def sleep_to_the_beginning_of_the_next_school_day():
-    end = (
-        now().replace(hour=MINIMUM_TIMETABLE_SENDING_HOUR)
-        + datetime.timedelta(days=2 if today().weekday() == SATURDAY else 1)
+async def wait_until_minimum_timetable_sending_hour():
+    date = today()
+    date += datetime.timedelta(
+        # Skipping the saturday because there will be no timetable for sunday on
+        # saturday (because we don't have lessons on sunday)
+        days=2 if date.weekday() == FRIDAY else 1
     )
-    while True:
-        if now() < end:
-            await asyncio.sleep(0)
-        else:
-            return
+    await wait_until(datetime.datetime.combine(
+        date, datetime.time(hour=MINIMUM_TIMETABLE_SENDING_HOUR)
+    ))
 
 
 class Bot:
@@ -98,104 +63,35 @@ class Bot:
         self.vk_client = vk_client
         self.config = config
         self.netschoolapi_client = netschoolapi_client
-        self.cached_timetable_info: Optional[CachedTimetableInfo] = None
-        self.timetable_getting_lock = asyncio.Lock()
+        self.timetable_cacher = TimetableCacher(netschoolapi_client, vk_client)
 
     async def run(self):
-        asyncio.create_task(
-            self.check_timetable_periodically_and_send_it()
-        ).add_done_callback(
-            _future_done_callback
-        )
+        asyncio.create_task(self.check_timetable_periodically_and_send_it())
         print("Starting!")
         await self.vk_client.run_polling()
-
-    async def update_timetable(self, ignore_cached: bool):
-        async with self.timetable_getting_lock:
-            today_ = today()
-            timetable_date = today_ + datetime.timedelta(
-                # Skipping the sunday
-                days=2 if today_.weekday() == SATURDAY else 1
-            )
-            next_day_number = timetable_date.day
-            if (
-                ignore_cached
-                or not self.cached_timetable_info
-                or (
-                    self.cached_timetable_info.month_day_number
-                    != next_day_number
-                )
-            ):
-                for announcement in (
-                    await self.netschoolapi_client.announcements()
-                ):
-                    for attachment in announcement.attachments:
-                        match = TIMETABLE_ANNOUNCEMENT_TITLE_REGEX.fullmatch(
-                            attachment.name
-                        )
-                        if match:
-                            if int(match.group(1)) == next_day_number:
-                                file_buffer = (
-                                    await self.netschoolapi_client
-                                    .download_attachment_as_bytes(
-                                        attachment
-                                    )
-                                )
-                                image_cropper = ImageCropper(
-                                    PILImageModule.open(file_buffer)
-                                )
-                                file_buffer = BytesIO()
-                                format_ = attachment.name.split(".")[-1]
-                                image_cropper.crop().save(
-                                    file_buffer,
-                                    format=(
-                                        "jpeg" if format_ == "jpg" else format_
-                                    )
-                                )
-                                attachment_string = (
-                                    await vkbottle.PhotoMessageUploader(
-                                        api=self.vk_client.api
-                                    ).upload(file_buffer)
-                                )
-                                self.cached_timetable_info = (
-                                    CachedTimetableInfo(
-                                        month_day_number=next_day_number,
-                                        attachment_string=attachment_string,
-                                        date=timetable_date
-                                    )
-                                )
-                                return
-                            raise TimetableForTomorrowIsntFound
-                raise TimetableForTomorrowIsntFound
 
     async def check_timetable_periodically_and_send_it(self):
         while True:
             try:
-                await self.update_timetable_and_send_it_to_peer_id(
-                    self.config.class_chat_peer_id, ignore_cached=True
-                )
-            except TimetableForTomorrowIsntFound:
+                timetable = await self.timetable_cacher.download()
+            except TimetableNotFound:
                 if now().hour > NIGHT_HOUR:
-                    await sleep_to_the_beginning_of_the_next_school_day()
+                    await wait_until_minimum_timetable_sending_hour()
                 else:
                     await asyncio.sleep(
                         self.config.timetable_checking_delay_in_seconds
                     )
             else:
-                await sleep_to_the_beginning_of_the_next_school_day()
+                await self.send_timetable_to_peer_id(
+                    timetable, self.config.class_chat_peer_id
+                )
+                await wait_until_minimum_timetable_sending_hour()
 
-    async def update_timetable_and_send_it_to_peer_id(
-            self, peer_id: int, ignore_cached: bool):
-        await self.update_timetable(ignore_cached)
-        await self.send_timetable_to_peer_id(peer_id)
-
-    async def send_timetable_to_peer_id(self, peer_id: int):
+    async def send_timetable_to_peer_id(
+            self, timetable: Timetable, peer_id: int):
         await self.vk_client.api.messages.send(
-            attachment=self.cached_timetable_info.attachment_string,
-            message=(
-                "Расписание на "
-                + self.cached_timetable_info.date.strftime("%d.%m.%Y")
-            ),
+            attachment=timetable.attachment_string,
+            message="Расписание на " + timetable.date.strftime("%d.%m.%Y"),
             peer_id=peer_id,
             random_id=random.randint(-1_000_000, 1_000_000)
         )
@@ -205,14 +101,16 @@ class Bot:
             text = message.text[1:].casefold()
             if text == "расписание":
                 try:
-                    await self.update_timetable_and_send_it_to_peer_id(
-                        message.peer_id, ignore_cached=False
+                    timetable = (
+                        await self.timetable_cacher.get_from_cache_or_download()
                     )
-                except TimetableForTomorrowIsntFound:
-                    if self.cached_timetable_info:
-                        await self.send_timetable_to_peer_id(message.peer_id)
-                    else:
-                        await message.answer("Расписания пока нет!")
+                except TimetableNotFound:
+                    await message.answer("Расписания пока нет!")
+                else:
+                    await self.send_timetable_to_peer_id(
+                        timetable=timetable,
+                        peer_id=message.peer_id
+                    )
             elif text in ("помощь", "команды"):
                 await message.answer(HELP_MESSAGE)
 
